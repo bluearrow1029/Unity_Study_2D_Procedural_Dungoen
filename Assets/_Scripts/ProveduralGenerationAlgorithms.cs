a37b185f2efbb43587ee072ede52f9225f21b615
Young Woo Noh <nohyw98@gmail.com>
2024-05-24 04:07:42 +0900

    P8. Saving parameters to scriptable object

---
+++ Assets/_Scripts/ProveduralGenerationAlgorithms.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ProveduralGenerationAlgorithms
+{
+    public static HashSet<Vector2Int> SimpleRandomWalk(Vector2Int startPosition, int walkLength)
+    {
+        HashSet<Vector2Int> path = new HashSet<Vector2Int>();
+
+        path.Add(startPosition);
+        var previousPosition = startPosition;
+
+        for (int index = 0; index < walkLength; index++)
+        {
+            var newPosition = previousPosition + Direction2D.GetRandomCardinalDirection();
+            path.Add(newPosition);
+            previousPosition = newPosition;
+        }
+
+        return path;
+    }
+}
+
+public static class Direction2D
+{
+    public static List<Vector2Int> cardinalDirectionsList = new List<Vector2Int>
+    {
+        new Vector2Int(0,1), // Up
+        new Vector2Int(1,0), // Right
+        new Vector2Int(0,-1), // Down
+        new Vector2Int(-1,0) // Left
+    };
+
+    public static Vector2Int GetRandomCardinalDirection()
+    {
+        return cardinalDirectionsList[Random.Range(0, cardinalDirectionsList.Count)];
+    }
+}
---
+++ Assets/_Scripts/SimpleRandomWalkDungeonGenerator.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Random = UnityEngine.Random;
+
+public class SimpleRandomWalkDungeonGenerator : AbstractDungeonGenerator
+{
+    [SerializeField]
+    private SimpleRandomWalkSO randomWalkParameters;
+
+    protected override void RunProceduralGeneration()
+    {
+        HashSet<Vector2Int> floorPositions = RunRandomWalk();
+        tilemapVisualizer.Clear();
+        tilemapVisualizer.PaintFloorTiles(floorPositions);
+    }
+
+    protected HashSet<Vector2Int> RunRandomWalk()
+    {
+        var currentPosition = startPosition;
+        HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
+
+        for(int index = 0; index < randomWalkParameters.iterations; index++)
+        {
+            var path = ProveduralGenerationAlgorithms.SimpleRandomWalk(currentPosition, randomWalkParameters.walkLength);
+            floorPositions.UnionWith(path);
+
+            if (randomWalkParameters.startRandomlyEachIteration)
+            {
+                currentPosition = floorPositions.ElementAt(Random.Range(0, floorPositions.Count));
+            }
+        }
+        return floorPositions;
+    }
+}
