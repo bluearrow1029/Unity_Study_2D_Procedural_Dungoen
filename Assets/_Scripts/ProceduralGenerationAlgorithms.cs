3883532ac3fb62bdf83f2284b528dab22d486692
Young Woo Noh <nohyw98@gmail.com>
2024-05-26 15:35:53 +0900

    P14. Splitting Rooms
    수직 분할과 수평 분할 메소드 완성. 분할점 좌표를 지정한 이후, 분할점 기준으로 room1과 room2의 좌표를 지정하여 roomsQueue에 넣음.

--- Assets/_Scripts/ProceduralGenerationAlgorithms.cs
+++ Assets/_Scripts/ProceduralGenerationAlgorithms.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public static class ProceduralGenerationAlgorithms
 {
@@ -48,15 +49,15 @@ public static class ProceduralGenerationAlgorithms
             
             if(room.size.y >= minHeight && room.size.x >= minWidth)
             {
-                if(UnityEngine.Random.value < 0.5f)
+                if(Random.value < 0.5f)
                 {
                     if(room.size.y >= minHeight * 2)
                     {
-                        SplitHorizontally(minWidth, minHeight, roomsQueue, room);
+                        SplitHorizontally(minHeight, roomsQueue, room);
                     }
                     else if(room.size.x >= minWidth * 2)
                     {
-                        SplitVertically(minWidth, minHeight, roomsQueue, room);
+                        SplitVertically(minWidth, roomsQueue, room);
                     }
                     else if(room.size.x >= minWidth && room.size.y >= minHeight)
                     {
@@ -67,11 +68,11 @@ public static class ProceduralGenerationAlgorithms
                 {
                     if (room.size.x >= minWidth * 2)
                     {
-                        SplitVertically(minWidth, minHeight, roomsQueue, room);
+                        SplitVertically(minWidth, roomsQueue, room);
                     }
                     else if (room.size.y >= minHeight * 2)
                     {
-                        SplitHorizontally(minWidth, minHeight, roomsQueue, room);
+                        SplitHorizontally(minHeight, roomsQueue, room);
                     }
                     else if (room.size.x >= minWidth && room.size.y >= minHeight)
                     {
@@ -84,14 +85,24 @@ public static class ProceduralGenerationAlgorithms
         return roomsList;
     }
 
-    private static void SplitVertically(int minWidth, int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
+    private static void SplitVertically(int minWidth, Queue<BoundsInt> roomsQueue, BoundsInt room)
     {
-        throw new System.NotImplementedException();
+        var xSplit = Random.Range(1, room.size.x);
+        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(xSplit, room.min.y, room.min.z));
+        BoundsInt room2 = new BoundsInt(new Vector3Int(room.min.x + xSplit, room.min.y, room.min.z), new Vector3Int(room.size.x - xSplit, room.size.y, room.size.z));
+
+        roomsQueue.Enqueue(room1);
+        roomsQueue.Enqueue(room2);
     }
 
-    private static void SplitHorizontally(int minWidth, int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
+    private static void SplitHorizontally(int minHeight, Queue<BoundsInt> roomsQueue, BoundsInt room)
     {
-        throw new System.NotImplementedException();
+        var ySplit = Random.Range(1, room.size.y);  // (minHeight, room.size.y - minHeight)
+        BoundsInt room1 = new BoundsInt(room.min, new Vector3Int(room.size.x, ySplit, room.size.z));
+        BoundsInt room2 = new BoundsInt(new Vector3Int(room.min.x, room.min.y + ySplit, room.min.z), new Vector3Int(room.size.x, room.size.y - ySplit, room.size.z));
+
+        roomsQueue.Enqueue(room1);
+        roomsQueue.Enqueue(room2);
     }
 }
 
